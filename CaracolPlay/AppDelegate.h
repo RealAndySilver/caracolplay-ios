7aad0e03b80e74bbafad855d5c9e6c2ffba70ade
Andres Abril <andres.abril@gmail.com>
2014-01-20 18:54:49 -0500

    First commit. Adding all files

---
+++ CaracolPlay/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CaracolPlay
+//
+//  Created by Andres Abril on 20/01/14.
+//  Copyright (c) 2014 iAmStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
