46efc006a37f2dcffb345ad7c908381c44d9a9e7
Diego Vidal <diefervidal91@gmail.com>
2014-12-11 17:18:39 -0500

    //Pequeños cambios en la reproducción del video

--- CaracolPlay/VideoPlayerViewController.m
+++ CaracolPlay/VideoPlayerViewController.m
@@ -18,16 +18,13 @@
 #import "BCOVWidevine.h"
 
 @interface VideoPlayerViewController () <ServerCommunicatorDelegate, BCOVPlaybackControllerDelegate>
-//@property (strong, nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
 @property (assign, nonatomic) BOOL sendProgressSecToServer;
 @property (strong, nonatomic) id <BCOVPlaybackController> controller;
+@property (assign, nonatomic) int progressTime;
 @end
 
 @implementation VideoPlayerViewController
 
-//NSString * const EMBED_CODE = @"1xZHNqazrsqfsHoMSjFk7Run5dd0DxKT";
-//NSString * const PCODE = @"n728cv9Ro-9N2pIPcA0vqCPxI_1yuaWcz1XaEpkc";
-//NSString * const PLAYERDOMAIN = @"www.ooyala.com";
 NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_qw..";
 
 -(void)viewDidLoad {
@@ -65,11 +62,10 @@ NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_q
     
     // add the playback controller
     BCOVPlayerSDKManager *manager = [BCOVPlayerSDKManager sharedManager];
-    //self.controller = [manager createPlaybackControllerWithViewStrategy:[self viewStrategy]];
     self.controller = [manager createWidevinePlaybackControllerWithViewStrategy:[self viewStrategy]];
     self.controller.view.frame = self.view.bounds;
     // create a playback controller delegate
-    //self.controller.delegate = self;
+    self.controller.delegate = self;
     
     self.controller.view.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
     // add the controller view as a subview of the SVPViewController's view
@@ -81,27 +77,10 @@ NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_q
     // add the video array to the controller's playback queue
     //[self.controller setVideos:videos];
     
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDetected)];
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDetected:)];
     [self.controller.view addGestureRecognizer:tapGesture];
     
     BCOVCatalogService *service = [[BCOVCatalogService alloc] initWithToken:TOKEN];
-    /*[service findVideoWithVideoID:@"3934819615001" parameters:nil completion:^(BCOVVideo *video, NSDictionary *jsonResponse, NSError *error) {
-        if (!error) {
-            NSLog(@"Response: %@", jsonResponse);
-            if (video) {
-                NSLog(@"The video exists");
-                [self.controller setVideos:@[video]];
-                [self.controller play];
-            } else {
-                NSLog(@"The video doesn't exist");
-            }
-            
-        } else {
-            NSLog(@"Error description: %@", [error localizedDescription]);
-            NSLog(@"Response: %@", jsonResponse);
-        }
-    }];*/
-    
     //3936114092001
     [service findWidevineVideoWithVideoID:self.embedCode parameters:nil completion:^(BCOVVideo *video, NSDictionary *jsonResponse, NSError *error) {
         if (!error) {
@@ -109,7 +88,7 @@ NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_q
             if (video) {
                 NSLog(@"The video exists");
                 [self.controller setVideos:@[video]];
-                [self.controller play];
+                //[self.controller play];
             } else {
                 NSLog(@"The video doesn't exist");
             }
@@ -121,21 +100,23 @@ NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_q
     }];
 }
 
--(void)tapDetected {
-    //NSLog(@"Detecté el tap");
-    NSArray *subViews = [self.controller.view subviews];
-    NSLog(@"EL número de subviews: %lu", (unsigned long)[subViews count]);
-    UIView *subview = subViews[1];
-    
-    static BOOL navigationBarHidden = NO;
-    if (!navigationBarHidden) {
-        [self.navigationController setNavigationBarHidden:YES animated:YES];
-        subview.hidden = YES;
-        navigationBarHidden = YES;
-    } else {
-        [self.navigationController setNavigationBarHidden:NO animated:YES];
-        navigationBarHidden = NO;
-        subview.hidden = NO;
+-(void)tapDetected:(UITapGestureRecognizer *)tap {
+    if ([tap locationInView:self.view].y < self.view.bounds.size.height - 60.0) {
+        NSLog(@"Detecté el tap");
+        NSArray *subViews = [self.controller.view subviews];
+        NSLog(@"EL número de subviews: %lu", (unsigned long)[subViews count]);
+        UIView *subview = subViews[1];
+        
+        static BOOL navigationBarHidden = NO;
+        if (!navigationBarHidden) {
+            [self.navigationController setNavigationBarHidden:YES animated:YES];
+            subview.hidden = YES;
+            navigationBarHidden = YES;
+        } else {
+            [self.navigationController setNavigationBarHidden:NO animated:YES];
+            navigationBarHidden = NO;
+            subview.hidden = NO;
+        }
     }
 }
 
@@ -149,10 +130,10 @@ NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_q
     self.controller = nil;
 }
 
--(void)viewWillLayoutSubviews {
+/*-(void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
     //self.ooyalaPlayerViewController.view.frame = self.view.bounds;
-}
+}*/
 
 -(void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
@@ -162,9 +143,6 @@ NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_q
     }
     self.tabBarController.tabBar.alpha = 1.0;
     [self.controller pause];
-    //[self.ooyalaPlayerViewController.player pause]; hay que quitar este coment
-    //[self.ooyalaPlayerViewController removeFromParentViewController]; este si hay que dejarlo
-    //NSLog(@"tiempo actual: %f", self.ooyalaPlayerViewController.player.playheadTime);
 }
 
 - (BCOVVideo *)videoWithURL:(NSURL *)url
@@ -197,11 +175,11 @@ NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_q
 
 -(void)postProgressSecToServer {
     //FIXME: no está funcionando la petición
-    /*ServerCommunicator *serverCommunicator = [[ServerCommunicator alloc] init];
+    ServerCommunicator *serverCommunicator = [[ServerCommunicator alloc] init];
     serverCommunicator.delegate = self;
-    NSString *parameters = [NSString stringWithFormat:@"%@/%d", self.productID, (int)self.ooyalaPlayerViewController.player.playheadTime];
+    NSString *parameters = [NSString stringWithFormat:@"%@/%d", self.productID, self.progressTime];
     [serverCommunicator callServerWithGETMethod:@"VideoWatched" andParameter:parameters];
-    NSLog(@"parámetros: %@", parameters);*/
+    NSLog(@"parámetros: %@", parameters);
 }
 
 -(void)receivedDataFromServer:(NSDictionary *)responseDictionary withMethodName:(NSString *)methodName {
@@ -221,10 +199,10 @@ NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_q
 
 #pragma mark - Notification Handlers
 
--(void)videoStartedPlaying {
+/*-(void)videoStartedPlaying {
     NSLog(@"el video empezó a reproducirse");
     self.sendProgressSecToServer = YES;
-}
+}*/
 
 -(NSUInteger)supportedInterfaceOrientations {
     return UIInterfaceOrientationMaskAll;
@@ -234,16 +212,20 @@ NSString * const TOKEN = @"23n6CnmhPeRe86DDzyGEpd49MDVnmYzUkSUqGaVv2oDVJSgcev5_q
 
 -(void)playbackController:(id<BCOVPlaybackController>)controller playbackSession:(id<BCOVPlaybackSession>)session didProgressTo:(NSTimeInterval)progress {
     //NSLog(@"Progresss: %f", progress);
+    self.progressTime = (int)progress;
 }
 
-/*-(void)playbackController:(id<BCOVPlaybackController>)controller playbackSession:(id<BCOVPlaybackSession>)session didReceiveLifecycleEvent:(BCOVPlaybackSessionLifecycleEvent *)lifecycleEvent {
+-(void)playbackController:(id<BCOVPlaybackController>)controller playbackSession:(id<BCOVPlaybackSession>)session didReceiveLifecycleEvent:(BCOVPlaybackSessionLifecycleEvent *)lifecycleEvent {
+    NSLog(@"se me llamó el delegateeeeeee***************************************");
     if ([lifecycleEvent.eventType isEqualToString:kBCOVPlaybackSessionLifecycleEventReady]) {
+        NSLog(@"Listooooooooooo para empezar el video");
         AVPlayer *player = session.player;
-        [player seekToTime:CMTimeMakeWithSeconds(8, 1) completionHandler:^(BOOL finished) {
+        [player seekToTime:CMTimeMakeWithSeconds(self.progressSec, 1) completionHandler:^(BOOL finished) {
+            NSLog(@"Plaayayayyyyyyyyyyy");
             [player play];
         }];
     }
-}*/
+}
 
 /*-(void)notificacion:(NSNotification *)notification {
  NSLog(@"Llegó la notificación");
